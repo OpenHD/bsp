6e4dfb318ac44ff28a6cfbbb59303d10be3ad6e2
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-10-30 18:07:22 +0100

    Update overlays.sh

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    #git_source https://github.com/radxa/overlays.git 0dcc88e63b008bf7d29646e56ec96c58bbe96414
-    #cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
+    git_source https://github.com/OpenHD/overlays_radxa/ 2911ebd0206721868451a026cac198662f7c63ab
+    cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
