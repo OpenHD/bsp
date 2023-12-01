b7b120dcdaf45894108bd88f5b6dc436c88d8925
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-12-02 00:12:59 +0100

    imx708 updates

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/openhd/overlays.git 88386e9ee2dd6549c1801e80879e12d90c6f39df
+    git_source https://github.com/openhd/overlays.git 385d1b0f4d3d7b72e0d77c37a0422415067253e1
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
