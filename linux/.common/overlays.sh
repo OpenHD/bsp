bc8a642dcf1bc3144dea6ce2131562df2910fed2
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-12-05 17:03:11 +0100

    zero3 imx708 test

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/openhd/overlays.git 385d1b0f4d3d7b72e0d77c37a0422415067253e1
+    git_source https://github.com/openhd/overlays.git 08e75ffd1bb1839f116cb8b45178c08e50d656da
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
