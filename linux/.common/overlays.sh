19f196105d6b6562b6c9cd81f8c35a24d2bb64a2
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-01-08 02:09:23 +0100

    Update overlays.sh

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/openhd/overlays.git e9527978e60864151f35daf99198fc64b99a40fb
+    git_source https://github.com/openhd/overlays.git ecb04707f19f74ed021f114c99ace08b39a8ce2a
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
